45c1e61e6842ab6fd907595b85f3036d0867cab8
Shasank Prasad <46317225+shasankp000@users.noreply.github.com>
2022-08-16 21:31:33 +0530

    Update install.py

--- install.py
+++ install.py
@@ -48,7 +48,7 @@ elif os_name.startswith("Windows"):
         os.remove(filename)
     else:
         os.system("cls")
-        print("Installing java 16.....")
+        print("Installing java 17.....")
         wget.download("https://download.bell-sw.com/java/17.0.3+7/bellsoft-jdk17.0.3+7-windows-amd64.msi", bar=wget.bar_adaptive)
         filename = wget.detect_filename("https://download.bell-sw.com/java/16.0.2+7/bellsoft-jdk16.0.2+7-windows-amd64.msi")
         os.system(f"msiexec /i {filename}")

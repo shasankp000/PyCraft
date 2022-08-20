b49fb819b627eeeb6ff082915a78b3d15d8fff68
Domenico <50798132+Domefemia@users.noreply.github.com>
2022-08-20 22:25:19 +0200

    Made the code more pretty

--- install.py
+++ install.py
@@ -17,7 +17,8 @@ if os_name.startswith("Linux"):
     os.system("clear")
     os.system("python3 -m pip install -r requirements.txt")
     os.system("sudo apt-get install gksudo fonts-symbola autoconf automake libtool gcc tor gtk2-engines-murrine python3 -y")
-    os.system("git clone https://git.torproject.org/torsocks.git") # Fix for torsocks syscall issue 217
+    # Fix for torsocks syscall issue 217
+    os.system("git clone https://git.torproject.org/torsocks.git")
     os.chdir("torsocks")
     os.system("./autogen.sh")
     os.system("./configure")
@@ -36,28 +37,33 @@ elif os_name.startswith("Windows"):
     os.system("pip install -r requirements.txt")
     os.system("cls")
     os.chdir(f"C:\\Users\\{usr_accnt}\\Downloads\\ ")
-    
+
     print("Do you want to install python? It is necessary for the launcher to run. (y/n)")
-    c1 = input("Enter y if you don't have python installed, n if you have it already : ")
+    c1 = input(
+        "Enter y if you don't have python installed, n if you have it already : ")
     if c1 == "y":
         print("Installing python 3.9.6.....")
-        wget.download("https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe", bar=wget.bar_adaptive)
-        filename = wget.detect_filename("https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe")
+        wget.download(
+            "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe", bar=wget.bar_adaptive)
+        filename = wget.detect_filename(
+            "https://www.python.org/ftp/python/3.9.6/python-3.9.6-amd64.exe")
         os.system(filename)
         time.sleep(5)
         os.remove(filename)
     else:
         os.system("cls")
         print("Installing java 17.....")
-        wget.download("https://download.bell-sw.com/java/17.0.3+7/bellsoft-jdk17.0.3+7-windows-amd64.msi", bar=wget.bar_adaptive)
-        filename = wget.detect_filename("https://download.bell-sw.com/java/16.0.2+7/bellsoft-jdk16.0.2+7-windows-amd64.msi")
+        wget.download(
+            "https://download.bell-sw.com/java/17.0.3+7/bellsoft-jdk17.0.3+7-windows-amd64.msi", bar=wget.bar_adaptive)
+        filename = wget.detect_filename(
+            "https://download.bell-sw.com/java/16.0.2+7/bellsoft-jdk16.0.2+7-windows-amd64.msi")
         os.system(f"msiexec /i {filename}")
     time.sleep(5)
     os.remove(f"{filename}")
     print("Creating desktop shortcut.....")
     os.chdir(f"C:\\Users\\{usr_accnt}\\Desktop\\ ")
-    os.system(f"shortcut.exe /F:Pycraft.lnk /A:C /T:{currn_dir}\\pycraft_gui.py /W:{currn_dir}")
+    os.system(
+        f"shortcut.exe /F:Pycraft.lnk /A:C /T:{currn_dir}\\pycraft_gui.py /W:{currn_dir}")
     os.system("cls")
     print("You can set the icon of the shortcut with the icon in pycraft's folder.")
     print("All requirements installed. Run pycraft_gui.py now to run the launcher.")
-

7c2eb856508628e5ad41ef2d8f2ed37e3913e584
Domenico <50798132+Domefemia@users.noreply.github.com>
2022-08-20 22:28:30 +0200

    Made the code prettier

--- speedtracker.py
+++ speedtracker.py
@@ -1,18 +1,17 @@
 import speedtest
 
 
-
 class SpeedTracker():
     '''A net speed tracker based on the speedtest-cli module'''
+
     def __init__(self):
         '''Gets the download speed in Mbit/s'''
         print("Checking the download speed....")
         self.raw_dl_speed = speedtest.Speedtest().download()
         self.rounded_speed = round(self.raw_dl_speed)
-        self.finalspeed = self.rounded_speed/(1e+6) 
-        
+        self.finalspeed = self.rounded_speed/(1e+6)
+
     def get_download_speed(self):
         '''Returns the download speed'''
-        print(f"Download Speed : {self.finalspeed} Mbit/s" )
-        return f"Download Speed : {self.finalspeed} Mbit/s" 
-
+        print(f"Download Speed : {self.finalspeed} Mbit/s")
+        return f"Download Speed : {self.finalspeed} Mbit/s"

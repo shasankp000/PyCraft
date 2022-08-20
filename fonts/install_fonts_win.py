0db801ee266d2506f40c065efc019ec004ade89c
Domenico <50798132+Domefemia@users.noreply.github.com>
2022-08-20 22:29:04 +0200

    Made the code prettier

--- fonts/install_fonts_win.py
+++ fonts/install_fonts_win.py
@@ -3,26 +3,26 @@
 import os
 import subprocess
 import time
-    
-    # vb script template
-TEMPL  = """ 
+
+# vb script template
+TEMPL = """ 
 Set objShell = CreateObject("Shell.Application")
 Set objFolder = objShell.Namespace("%s")
 Set objFolderItem = objFolder.ParseName("%s")
 objFolderItem.InvokeVerb("Install")
 """
-    
-    
+
+
 vbspath = os.path.join(os.getcwd(), 'fontinst.vbs')
-    
+
 for directory, dirnames, filenames in os.walk(os.getcwd()):
     for filename in filenames:
         fpath = os.path.join(directory, filename)
-    
-        if fpath[-4:] == ".ttf": # modify this line for including multiple extension
+
+        if fpath[-4:] == ".ttf":  # modify this line for including multiple extension
             with open(vbspath, 'w') as _f:
-                f.write(_TEMPL%(directory, filename))
+                f.write(_TEMPL % (directory, filename))
                 subprocess.call(['cscript.exe', vbspath])
-                time.sleep(3) # can omit this
-                    
+                time.sleep(3)  # can omit this
+
 os.remove(vbspath)  # clean
